64e445b5d2348cd37265213c088e73f041e0fe10
Jay Bhatt <jkb401.jb@gmail.com>
2021-05-15 14:57:02 +0530

    Complete app v1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 import ChatScreen from './ChatScreen';
 import Sidebar from './Sidebar';
+import Pusher from 'pusher-js';
+import axios from './axios'
+
 
 function App() {
+  const [messages, setMessges] = useState([]);
+
+  useEffect(()=> {
+    axios.get('/messages/sync').then(response => {
+      console.log(response.data);
+      setMessges(response.data);
+    })
+  },[]);
+
+  useEffect(()=> {
+    const pusher = new Pusher('b270a904cc54b4bdfd48', {
+      cluster: 'ap2'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', function(data) {
+      // alert(JSON.stringify(data));
+      setMessges([...messages,data]);
+    });
+
+    return () => { 
+      channel.unbind_all();
+      channel.unsubscribe();
+    }
+  },[messages]);
+
+  console.log(messages);
   return (
     <div className="app">
      <div className='app__greenHeader'>
@@ -11,7 +42,7 @@ function App() {
      </div>
      <div className='app__main'>
         <Sidebar />
-        <ChatScreen /> 
+        <ChatScreen messages = {messages} /> 
 
      </div>
      
--- src/ChatScreen.js
+++ src/ChatScreen.js
@@ -1,11 +1,24 @@
 import { Avatar, IconButton } from '@material-ui/core'
-import React from 'react'
+import React, { useState } from 'react'
 import './ChatScreen.css'
 import SearchIcon from '@material-ui/icons/Search';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { AttachFile, InsertEmoticon, Mic, Send } from '@material-ui/icons';
+import axios from './axios'
 
-function ChatScreen() {
+function ChatScreen({messages}) {
+    const [input, setInput] = useState("");
+    const sendMessage = async (e) => {
+        e.preventDefault();
+        await axios.post('/messages/new',{
+            message: input,
+            name: "Set user",
+            timestamp: "Just now",
+            received: false.valueOf,
+
+        });
+        setInput('');
+    };
     return (
         <div className='chatScreen'>
             <div className='chatScreen__header'>
@@ -27,25 +40,14 @@ function ChatScreen() {
                 </div>
             </div>
             <div className='chatScreen__body'>
-
-                <p className='chat__message'>
-                    <span className='chat__name'>Jay</span>
-                    This is a message.
-                    <span className='chat__timestamp'>{new Date().toLocaleString()}</span>
-                </p>
-
-                <p className='chat__message'>
-                    <span className='chat__name'>Jay</span>
-                    This is a message.
-                    <span className='chat__timestamp'>{new Date().toLocaleString()}</span>
-                </p>
-
-                <p className='chat__message'>
-                    <span className='chat__name'>Jay</span>
-                    This is a message.
-                    <span className='chat__timestamp'>{new Date().toLocaleString()}</span>
-                </p>
-
+                {messages.map(message => (
+                     <p className= {`chat__message ${message.received && 'chat__receiver'} `}>
+                     <span className='chat__name'>{message.name}</span>
+                     {message.message}
+                     <span className='chat__timestamp'>{message.timestamp}</span>
+                     </p>
+                ))}
+               
             </div>
             <div className='chatScreen__footer'>
                 <div className='footer__left'>
@@ -55,8 +57,8 @@ function ChatScreen() {
                 
                 <div className='footer__center'>
                     <form>
-                        <input className='chat__input' type='text' placeholder='Type a message...'></input>
-                        <button type='submit'></button>
+                        <input  value={input} onChange={e => setInput(e.target.value)} className='chat__input' type='text' placeholder='Type a message...'></input>
+                        <button onClick={sendMessage} type='submit'></button>
                     </form>
                 </div>
                 <div className='footer__right'>
